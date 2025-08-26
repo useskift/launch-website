e85b53dbd5c3b5709061acb28764a1ec5af8985d
William Hallin <william@hallin.media>
2025-08-26 09:58:05 +0200

    refactor: remove @sentry/cloudflare integration and update Sentry configuration

--- functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/cloudflare";
-
-export const onRequest = [
-  Sentry.sentryPagesPlugin((context) => ({
-    dsn: "https://dde6dc3a09940e5fb15349c116018894@o4509508005134336.ingest.de.sentry.io/4509544307425360",
-    tracesSampleRate: 0.1,
-  })),
-];
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,15 +2,8 @@ import * as Sentry from "@sentry/astro";
 
 Sentry.init({
   dsn: "https://30e19654baa2c41a091a45c5639a6325@o4509508005134336.ingest.de.sentry.io/4509555522142288",
+
   sendDefaultPii: true,
-
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-
   enableLogs: true,
   tracesSampleRate: 0.5,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
 });
