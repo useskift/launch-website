50aa8fae29e4159fc6ca4f23751aa425515d225d
William Hallin <william@hallin.media>
2025-08-29 12:29:44 +0200

    refactor: remove Sentry server configuration file

--- sentry.server.config.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/astro";
-
-Sentry.init({
-  dsn: "https://30e19654baa2c41a091a45c5639a6325@o4509508005134336.ingest.de.sentry.io/4509555522142288",
-
-  sendDefaultPii: true,
-  enableLogs: true,
-  tracesSampleRate: 0.5,
-});
